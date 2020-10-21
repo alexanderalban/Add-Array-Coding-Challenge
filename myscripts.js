5e2a74020ca819cad64c9c8e582983004be0cf26
alexanderalban <albanalexander@gmail.com>
2020-10-20 19:35:04 -0500

    lesson follow along

--- myscripts.js
+++ myscripts.js
@@ -1,15 +1,13 @@
-const arr = [2, 5, 6, 7, 9]
+const exampleArray = [2, 5, 6, 7, 9]
 
-const addOne = (newArray) => {
-    let newList = [];
-    let sum = 0;
-    for (let i = 0; i < newArray.length; i++) {
-        sum += (newArray[i] + 1);
-        console.log('sum = ', sum);
-        newList.push(newArray[i] + 1)
-        console.log("newList =", newList);
-    }
-    return newList + ' ' + sum
+const addOneWithSum = (ourArray) => {
+  let newList = []
+  let sum = 0
+  for (let i = 0; i < ourArray.length; i++) {
+    sum += (ourArray[i] + 1)
+    newList.push(ourArray[i] + 1)
+  }
+  return newList + ' ' + sum
 }
 
-addOne(arr);
+addOneWithSum(exampleArray)
